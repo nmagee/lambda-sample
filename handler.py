078bfd1f8440485c41eeeeb0d7d25e44f5e8cb7a
Neal Magee <nem2p@virginia.edu>
2020-08-13 16:26:14 -0400

    First real commit

---
+++ handler.py
@@ -0,0 +1,13 @@
+import json
+import boto3
+
+
+# Catch an event, parse it, and do something with it.
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
